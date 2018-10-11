f8d1ecd7b7069bff0a23eb742bac64aa169f8ab4
William Gregory <william.gregory@acadiasoft.com>
2018-10-11 11:21:15 +0100

    Prettier linting

--- test/index.js
+++ test/index.js
@@ -4,10 +4,10 @@ import assert from 'assert';
 import '../lib/index.js';
 
 describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
-      assert.equal(200, res.statusCode);
-      done();
+    it('should return 200', (done) => {
+        http.get('http://127.0.0.1:1337', (res) => {
+            assert.equal(200, res.statusCode);
+            done();
+        });
     });
-  });
 });
